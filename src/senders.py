d64950f29d9408472ba9d04b2759908f6bdf6483
chraulley <chenglyes@163.com>
2026-04-12 16:52:20 +0800

    add telegram bot sender

--- src/senders.py
+++ src/senders.py
@@ -24,6 +24,8 @@ def create_sender(name: str, *args, **kwargs) -> Sender:
         return SaveToFileSender(name, *args, **kwargs)
     if name == "server_chan":
         return ServerChanSender(name, *args, **kwargs)
+    if name == "telegram_bot":
+        return TelegramBotSender(name, *args, **kwargs)
     return Sender(name)
 
 class SaveToFileSender(Sender):
@@ -87,4 +89,33 @@ class ServerChanSender(Sender):
         code = result.get("code", -1)
         if code != 0:
             raise RuntimeWarning(f"return code '{code}', response '{result}'")
-        
+
+class TelegramBotSender(Sender):
+    def __init__(self, name: str, bot_token: str | None = None, chat_id: str | None = None) -> None:
+        super().__init__(name)
+        if not bot_token:
+            bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+        if not chat_id:
+            chat_id = os.getenv("TELEGRAM_CHAT_ID")
+        if not bot_token:
+            logging.warning(f"sender '{name}': bot_token is None, please config 'bot_token' or environmental variable 'TELEGRAM_BOT_TOKEN'")
+        if not chat_id:
+            logging.warning(f"sender '{name}': chat_id is None, please config 'chat_id' or environmental variable 'TELEGRAM_CHAT_ID'")
+        self.bot_token = bot_token
+        self.chat_id = chat_id
+
+    def _send(self, paper: Paper):
+        if not self.bot_token or not self.chat_id:
+            raise RuntimeWarning("no bot_token or chat_id")
+        url = f"https://api.telegram.org/bot{self.bot_token}/sendMessage"
+        title = paper.title_translation if paper.title_translation else paper.title
+        text = f"*{title}*\n\n{paper.to_markdown()}"
+        params = {
+            "chat_id": self.chat_id,
+            "text": text,
+            "parse_mode": "Markdown",
+        }
+        response = requests.post(url, json=params)
+        result = response.json()
+        if not result.get("ok"):
+            raise RuntimeWarning(f"telegrambot error, response '{result}'")
